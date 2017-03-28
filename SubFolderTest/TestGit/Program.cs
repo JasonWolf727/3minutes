f8911c0fa35338b0af01ccb69d69e0d040c7c01d
mjoha563 <mjoha563@students.bju.edu>
2017-03-28 15:47:54 -0400

    Create Window and Close Button

--- SubFolderTest/TestGit/Program.cs
+++ SubFolderTest/TestGit/Program.cs
@@ -12,7 +12,41 @@ namespace TestGit
 {
     class Program : SimpleApplication
     {
+        Window menu;
+        Button close;
+        UIElement uiRoot;
+        Font font;
+
         Program(ApplicationOptions options = null) :base(options) { }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            var cache = ResourceCache;
+
+            XmlFile style = cache.GetXmlFile("UI/DefaultStyle.xml");
+            font = cache.GetFont("Fonts/Anonymous Pro.ttf");
+            
+            uiRoot = UI.Root;
+            uiRoot.SetDefaultStyle(style);
+
+            menu = uiRoot.CreateWindow("MyWindow", 1);
+            menu.SetStyleAuto(null);
+            menu.SetAlignment(HorizontalAlignment.Center, VerticalAlignment.Center);
+            menu.SetMinSize(300, 600);
+            menu.Visible = true;
+
+            close = menu.CreateButton("Close", 1);
+            close.SetStyleAuto(null);
+            close.SetMinSize(100, 30);
+            close.SetMaxSize(100, 30);
+            close.SetAlignment(HorizontalAlignment.Center, VerticalAlignment.Bottom);
+
+            close.SubscribeToReleased(_=>Exit());
+
+        }
+
         static void Main(string[] args)
         {
             var app = new Program(new ApplicationOptions("Data")
