2dafc466e168b90e5a509292013161593ce503e9
mjoha563 <mjoha563@students.bju.edu>
2017-03-28 14:56:47 -0400

    Change 2

--- SubFolderTest/TestGit/Program.cs
+++ SubFolderTest/TestGit/Program.cs
@@ -5,15 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using Urho;
 using Urho.Gui;
+using Urho.Resources;
 using Urho.Shapes;
 
-namespace TestGit
+namespace TestGit 
 {
-    class Program
+    class Program : SimpleApplication
     {
+        Program(ApplicationOptions options = null) :base(options) { }
         static void Main(string[] args)
         {
+            var app = new Program(new ApplicationOptions("Data")
+            {
+                TouchEmulation = true,
+                WindowedMode = true
+            });
+            app.Run();
         }
+
 
         public void MyNoReturnMethod()
         {
