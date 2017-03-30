8b31704366bf600df085318fe2d89048bf64499b
csant714 <csant714@bju.edu>
2017-03-30 19:50:16 -0400

    Resolve the Conflict

--- CC-X/CC-X/Model/HighScore.cs
+++ CC-X/CC-X/Model/HighScore.cs
@@ -8,14 +8,6 @@ namespace CC_X.Model
 {
     class HighScore
     {
-        //public void Serialize()
-        //{
-        //    // Take the stuff in World and put it into a single comma-delimited string.
-        //}
-
-        //public void DeSerialize()
-        //{
-        //    // Take the stuff in a CSV file and put it into a single comma-delimited string; then distribute among all the values.
-        //}
+        
     }
 }
