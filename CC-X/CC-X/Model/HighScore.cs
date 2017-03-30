23793d4f8031ff10d383791da421fbedb91c2590
jcase146 <jcase146@students.bju.edu>
2017-03-30 19:43:13 -0400

    HighScore write file

--- CC-X/CC-X/Model/HighScore.cs
+++ CC-X/CC-X/Model/HighScore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace CC_X.Model
 {
     class HighScore
     {
+        const string highScoreFile = "highscore.txt";
+        int timesTest = 0;
+        public void WriteToFile()
+        {
+            using (StreamWriter writer = new StreamWriter(highScoreFile))
+            {
+                writer.Write("test" + timesTest);
+            }
+            timesTest += 1;
+        }
     }
 }
--- CC-X/CC-X/Program.cs
+++ CC-X/CC-X/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CC_X.Model;
 
 namespace CC_X
 {
