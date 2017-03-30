d2c935534ac98080ac0890d280210fa8440389a3
mjoha563 <mjoha563@students.bju.edu>
2017-03-30 12:46:54 -0400

    Start design for World Objects and edit GameController as necessary

--- CC-X/CC-X/Model/Enemies.cs
+++ CC-X/CC-X/Model/Enemies.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace CC_X.Model
 {
-    class Enemies
+    class Enemies : World
     {
+        public Enemies()
+        {
+
+        }
+        public override void UpdatePos(int X, int Y, int Z)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
