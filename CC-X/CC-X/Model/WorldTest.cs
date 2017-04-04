d5d0a7089c825e92bb210e29350606d34010b562
mjoha563 <mjoha563@students.bju.edu>
2017-04-04 18:10:38 -0400

    Adjust Unit Tests to Model Obj changes

--- CC-X/CC-X/Model/WorldTest.cs
+++ CC-X/CC-X/Model/WorldTest.cs
@@ -13,9 +13,9 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_zombieMainChar_NoCollision()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 2, 1);
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(5, 4, 1);
 
             Dictionary<int, GameObj> world = new Dictionary<int, GameObj>();
@@ -27,9 +27,9 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_zombieMainCharClosePos_Collision()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 2, 3);
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(0.95f, 4, 3);
 
             Dictionary<int, GameObj> world = new Dictionary<int, GameObj>();
@@ -41,9 +41,9 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_zombieMainCharEqualPosX_Collision()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 2, 8);
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(1, 4, 8);
 
             Dictionary<int, GameObj> world = new Dictionary<int, GameObj>();
@@ -55,9 +55,9 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_zombieMainCharEqualPosY_Collision()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 4, 8);
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(3, 4, 8);
 
             Dictionary<int, GameObj> world = new Dictionary<int, GameObj>();
@@ -69,10 +69,10 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_AssessDamage_DamageTaken()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 4, 8);
             zombie.Damage = 20;
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(3, 4, 8);
             MainChar.Health = 100;
 
@@ -86,10 +86,10 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_AssessDamage_NoDamageTaken()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 4, 8);
             zombie.Damage = 20;
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(3, 2, 8);
             MainChar.Health = 100;
 
@@ -103,10 +103,10 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_AssessDamageClosePos_DamageTaken()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 4, 8);
             zombie.Damage = 20;
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(0.95f, 3.95f, 8);
             MainChar.Health = 100;
 
@@ -120,10 +120,10 @@ namespace CC_X.Model
         [TestMethod]
         public void DetectCollision_AssessDamageAndDeath_NoException()
         {
-            GameObj zombie = new Enemy();
+            Enemy zombie = new Enemy();
             zombie.Position = new Urho.Vector3(1, 4, 8);
             zombie.Damage = 20;
-            GameObj MainChar = new MainCharacter();
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(3, 4, 8);
             MainChar.Health = 100;
 
@@ -157,8 +157,8 @@ namespace CC_X.Model
         }
         [TestMethod]
         public void UpdatePos_MainChar_NoException()
-        {            
-            GameObj MainChar = new MainCharacter();
+        {
+            MainCharacter MainChar = new MainCharacter();
             MainChar.Position = new Urho.Vector3(0.95f, 3.95f, 8);
             Urho.Vector3 pos = new Urho.Vector3(1, 2, 3);
             MainChar.UpdatePos(pos);
@@ -177,7 +177,7 @@ namespace CC_X.Model
         [TestMethod]
         public void UpdatePos_Enemies_NoException()
         {
-            GameObj enemy = new Enemy();
+            Enemy enemy = new Enemy();
             enemy.Position = new Urho.Vector3(0.95f, 3.95f, 8);
             Urho.Vector3 pos = new Urho.Vector3(1, 2, 3);
             enemy.UpdatePos(pos);
