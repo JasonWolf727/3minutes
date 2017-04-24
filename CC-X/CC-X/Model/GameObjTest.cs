1859fd4edb78dd7a4d0428d8ef4fdecff6d62071
csant714 <csant714@bju.edu>
2017-04-24 19:00:21 -0400

    Comments for SerializationTest and GameObjTest

--- CC-X/CC-X/Model/GameObjTest.cs
+++ CC-X/CC-X/Model/GameObjTest.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * File: GameObjTest.cs
+ * Author: Michael Johannes
+ * Desc: Features unit tests for the Game Objects
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +16,14 @@ using Urho;
 
 namespace CC_X.Model
 {
+    // Unit tests using an instance of the Game Object Class
     [TestClass]
     public class GameObjTest
     {
         Dictionary<uint, GameObj> GameObjCollection { get; set; } // Contains Nature and Enemy objects      
-        MainCharacter MainChar = new MainCharacter(new Vector3(75, -0.50523f, 1.62f));
+        MainCharacter MainChar = new MainCharacter(new Vector3(75, -0.50523f, 1.62f)); // Instance of the Main Character class
 
+        // Logic to calculate if a collision has taken place. If so, do the damage thing. If not, reset the object.
         List<object> DetectCollision()
         {
             if (MainChar != null)
@@ -38,6 +46,8 @@ namespace CC_X.Model
                 return new List<object>() { false, new Vector3(-1000000, -1000000, -1000000) };
             }
         }
+
+        // Test to make sure the two objects are not colliding while being rendered.
         [TestMethod]
         public void DetectCollision_zombieMainChar_NoCollision()
         {
@@ -51,6 +61,7 @@ namespace CC_X.Model
             Assert.IsTrue((bool)(game.DetectCollision()[0]) != true);
         }
 
+        // Test to see if the Zombie collided with the Main Character
         [TestMethod]
         public void DetectCollision_zombieMainCharClosePos_Collision()
         {
@@ -64,6 +75,7 @@ namespace CC_X.Model
             Assert.IsTrue((bool)(DetectCollision()[0]) == true);
         }
 
+        // Make sure that two objects with equal X's collide
         [TestMethod]
         public void DetectCollision_zombieMainCharEqualPosX_Collision()
         {
@@ -75,6 +87,7 @@ namespace CC_X.Model
             Assert.IsTrue((bool)(DetectCollision()[0]) == true);
         }
 
+        // Make sure that two objects with equal Y's collide
         [TestMethod]
         public void DetectCollision_zombieMainCharEqualPosY_Collision()
         {
@@ -88,6 +101,7 @@ namespace CC_X.Model
             Assert.IsTrue((bool)(game.DetectCollision()[0]) == true);
         }
 
+        // Make sure the character's health was deducted correctly
         [TestMethod]
         public void DetectCollision_AssessDamage_DamageTaken()
         {            
@@ -104,6 +118,8 @@ namespace CC_X.Model
 
             Assert.IsTrue(MainChar.Health == 80);
         }
+
+        // Make sure the character's health didn't go down when the collision doesn't happen
         [TestMethod]
         public void DetectCollision_AssessDamage_NoDamageTaken()
         {
@@ -121,6 +137,8 @@ namespace CC_X.Model
 
             Assert.IsTrue(MainChar.Health == 100);
         }
+
+        // Make sure the damage deducts correctly in the event of proximity
         [TestMethod]
         public void DetectCollision_AssessDamageClosePos_DamageTaken()
         {
@@ -137,6 +155,8 @@ namespace CC_X.Model
 
             Assert.IsTrue(game.MainChar.Health == 80);
         }
+
+        // Check for continuing damage until death occurs
         [TestMethod]
         public void DetectCollision_AssessDamageAndDeath_NoException()
         {
@@ -175,6 +195,8 @@ namespace CC_X.Model
             Assert.IsTrue(game.MainChar.Health == 0);
             Assert.IsTrue(game.MainChar.IsDead == true);
         }
+
+        // Check for a correct update of position.
         [TestMethod]
         public void UpdatePos_MainChar_NoException()
         {
@@ -193,6 +215,8 @@ namespace CC_X.Model
             Assert.IsTrue(MainChar.Position.Y == 0);
             Assert.IsTrue(MainChar.Position.Z == 0);
         }
+
+        // Check for a correct update of Enemy position
         [TestMethod]
         public void UpdatePos_Enemies_NoException()
         {
--- CC-X/CC-X/Model/SerializationTest.cs
+++ CC-X/CC-X/Model/SerializationTest.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * File: SerializationTest.cs
+ * Author: Carlos Santana
+ * Desc: Contains unit tests for the (De)Serialization logic.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +13,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CC_X.Model
 {
+    // Class for the (De)Serialization Unit Tests
     [TestClass]
     public class SerializationTest
     {
 
+        // Tests the power of MainCharacter's Serializer method
         [TestMethod]
         public void MainCharSerializer()
         {
-            Urho.Vector3 pos = new Urho.Vector3(1, 2, 4);
-            MainCharacter chara = new MainCharacter(pos);
+            Urho.Vector3 pos = new Urho.Vector3(1, 2, 4); // Position for use in the instance of MainCharacter
+            MainCharacter chara = new MainCharacter(pos); // Instance of MainCharacter for use in the Unit Test
             chara.Position = pos;
             chara.ID = 42;
             chara.SelectedCharType = MainCharacter.MainCharOptn.Mutant;
@@ -23,17 +31,18 @@ namespace CC_X.Model
             chara.Health = 90;
             chara.TimeSinceLastCollide = 1337;
             chara.IsDead = false;
-            chara.persnlBubble = new System.Drawing.Rectangle(1, 2, 3, 4);
+            chara.persnlBubble = new System.Drawing.Rectangle(1, 2, 3, 4); // Set the instance's persnlBubble
             string tempInfo = chara.Serialize();
             Assert.IsTrue(tempInfo == "1, 2, 4, 42, Mutant, 64, 90, 1337, False, 1, 2, 3, 4");
         }
 
+        // Tests the power of MainCharacter's DeSerializer method
         [TestMethod]
         public void MainCharDeSerializer()
         {
             string tempInfo = "1, 2, 4, 42, Mutant, 64, 90, 1337, False, 1, 2, 3, 4";
-            Urho.Vector3 pos = new Urho.Vector3(1, 2, 4);
-            MainCharacter chara = new MainCharacter(pos);
+            Urho.Vector3 pos = new Urho.Vector3(1, 2, 4); // Position for use in the instance of MainCharacter
+            MainCharacter chara = new MainCharacter(pos); // Instance of MainCharacter for use in the Unit Test
             chara.DeSerialize(tempInfo);
             Assert.IsTrue(chara.Position == pos);
             Assert.IsTrue(chara.ID == 42);
@@ -42,15 +51,16 @@ namespace CC_X.Model
             Assert.IsTrue(chara.Health == 90);
             Assert.IsTrue(chara.TimeSinceLastCollide == 1337);
             Assert.IsTrue(chara.IsDead == false);
-            System.Drawing.Rectangle space = new System.Drawing.Rectangle(1, 2, 3, 4);
+            System.Drawing.Rectangle space = new System.Drawing.Rectangle(1, 2, 3, 4); // Set the instance's persnlBubble
             Assert.IsTrue(chara.persnlBubble == space);
         }
 
+        // Tests the power of Nature's Serializer method
         [TestMethod]
         public void NatureSerializer()
         {
-            Urho.Vector3 pos = new Urho.Vector3(5, 12, 13);
-            Nature flowey = new Nature(Nature.NatureType.Grass, pos);
+            Urho.Vector3 pos = new Urho.Vector3(5, 12, 13); // Position for use in the instance of Nature
+            Nature flowey = new Nature(Nature.NatureType.Grass, pos); // Instance of Nature for use in the Unit Test
             flowey.SelectedNatureType = Nature.NatureType.Grass;
             flowey.Position = pos;
             flowey.ID = 24;
@@ -58,42 +68,45 @@ namespace CC_X.Model
             Assert.IsTrue(tempInfo == "5, 12, 13, 24, Grass");
         }
 
+        // // Tests the power of Nature's DeSerializer method
         [TestMethod]
         public void NatureDeSerializer()
         {
             string tempInfo = "5, 12, 13, 24, Grass";
-            Urho.Vector3 pos = new Urho.Vector3(5, 12, 13);
-            Nature flowey = new Nature(Nature.NatureType.Grass, pos);
+            Urho.Vector3 pos = new Urho.Vector3(5, 12, 13); // Position for use in the instance of Nature
+            Nature flowey = new Nature(Nature.NatureType.Grass, pos); // Instance of Nature for use in the Unit Test
             flowey.DeSerialize(tempInfo);
             Assert.IsTrue(flowey.Position == pos);
             Assert.IsTrue(flowey.ID == 24);
             Assert.IsTrue(flowey.SelectedNatureType == Nature.NatureType.Grass);
         }
 
+        // Tests the power of Enemy's Serializer method
         [TestMethod]
         public void EnemySerializer()
         {
-            Urho.Vector3 pos = new Urho.Vector3(3, 4, 5);
-            Enemy froggit = new Enemy(pos);
+            Urho.Vector3 pos = new Urho.Vector3(3, 4, 5); // Position for use in the instance of Enemy
+            Enemy froggit = new Enemy(pos); // Instance of Enemy for use in the Unit Test
             froggit.Position = pos;
             froggit.ID = 65;
             froggit.ObjType = Enemy.EnemyType.Car;
             froggit.Strength = 2;
             froggit.Health = 30;
             froggit.IsDead = false;
-            froggit.persnlBubble = new System.Drawing.Rectangle(2, 5, 9, 7);
+            froggit.persnlBubble = new System.Drawing.Rectangle(2, 5, 9, 7); // Set the instance's persnlBubble
             froggit.CarMovingDirection = Enemy.CarDir.Right;
             string tempInfo = froggit.Serialize();
             Assert.IsTrue(tempInfo == "3, 4, 5, 65, Car, 2, 30, False, 2, 5, 9, 7, Right");
         }
 
+        // Tests the power of Enemy's DeSerializer method
         [TestMethod]
         public void EnemyDeSerializer()
         {
             string tempInfo = "3, 4, 5, 65, Car, 2, 30, False, 2, 5, 9, 7, Right";
-            Urho.Vector3 pos = new Urho.Vector3(3, 4, 5);
-            System.Drawing.Rectangle space = new System.Drawing.Rectangle(2, 5, 9, 7);
-            Enemy froggit = new Enemy(pos);
+            Urho.Vector3 pos = new Urho.Vector3(3, 4, 5); // Position for use in the instance of Enemy
+            System.Drawing.Rectangle space = new System.Drawing.Rectangle(2, 5, 9, 7); // Set the instance's persnlBubble
+            Enemy froggit = new Enemy(pos); // Instance of Enemy for use in the Unit Test
             froggit.DeSerialize(tempInfo);
             Assert.IsTrue(froggit.Position == pos);
             Assert.IsTrue(froggit.ID == 65);
@@ -103,22 +116,6 @@ namespace CC_X.Model
             Assert.IsTrue(froggit.IsDead == false);
             Assert.IsTrue(froggit.persnlBubble == space);
             Assert.IsTrue(froggit.CarMovingDirection == Enemy.CarDir.Right);
-        }
-
-        [TestMethod]
-        public void Saver()
-        {
-            Assert.IsTrue(false);
-            string filepath = "C:\\Users\\csant714\\Desktop\\3minutes\\CC-X\\CC-X\\Model\\LoadSaveTest.csv";
-            Console.WriteLine(filepath);
-        }
-
-        [TestMethod]
-        public void Loader()
-        {
-            Assert.IsTrue(false);
-            string filepath = "C:\\Users\\csant714\\Desktop\\3minutes\\CC-X\\CC-X\\Model\\LoadSaveTest.csv";
-            Console.WriteLine(filepath);
         }
     }
 }
